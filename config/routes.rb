5de38b4d5cde32b2ff69b95684a84c68bcab27d7
Alisson Rangel <alisson.rangel@hotmail.com>
2021-04-20 15:20:01 -0300

    1.5 e 1.6, criando o painel do admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, skip: [:registrations], controllers: {sessions: 'admins/sessions' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root to: "home#index"
+  
+  namespace :admin do
+    #admin/posts
+    root to: "home#index"
+  end
+  
 end
