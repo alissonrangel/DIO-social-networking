fb2d2a7fba1050f785c3cb51e8e2823dc49aedc2
Alisson Rangel <alisson.rangel@hotmail.com>
2021-04-22 20:04:03 -0300

    8.3, testando subscriptions

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -3,10 +3,23 @@ require 'rails_helper'
 RSpec.describe Subscription, type: :model do
   
   it "user can follow another user" do
-    matt = FactoryBot.create(:user)
-    puts(matt.name)
+    matt = FactoryBot.create(:user)    
     bob = FactoryBot.create(:user)
-    puts(bob.name)
-    Subscription.create(followed: bob, followed_by: matt)  
+    dave = FactoryBot.create(:user)    
+    Subscription.create(followed: bob, followed_by: matt)
+    Subscription.create(followed: bob, followed_by: dave)
+    Subscription.create(followed: dave, followed_by: bob)    
+
+    expect(bob.followers.count).to eql 2
+    expect(bob.followers).to include dave
+
+    expect(matt.following.count).to eql 1
+    expect(matt.following).to include bob
+
+    expect(bob.following.count).to eql 1
+    expect(bob.following).to_not include matt
+
+    expect(matt.followers.count).to eql 0
+    expect(matt.followers).to_not include bob
   end
 end
