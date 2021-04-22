3db1a3222c610c3e5d11648f651e19bb29e5609c
Alisson Rangel <alisson.rangel@hotmail.com>
2021-04-22 18:36:01 -0300

    7.4, criar posts e mostrar no painel do admin

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     root to: "home#index"
     resources :admins
     resources :users, only: :index
+    resources :posts, only: [:index, :show]
   end
 
   root to: "user/timeline#index"
