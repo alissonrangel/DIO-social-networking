114d6c5ef6e9eca0f39363b9700715130282a6e6
Alisson Rangel <alisson.rangel@hotmail.com>
2021-04-22 20:27:52 -0300

    9.1, criar view, rotas, action no profile_controller de potencial_to_follow

--- app/controllers/user/profile_controller.rb
+++ app/controllers/user/profile_controller.rb
@@ -2,4 +2,8 @@ class User::ProfileController < UserController
   def show
     @post = Post.new
   end
+
+  def potencial_to_follow
+    @potencial_to_follow = User.where.not(id: current_user.following.pluck(:id)).where.not(id: current_user.id)
+  end
 end
