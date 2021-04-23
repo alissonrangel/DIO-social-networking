d492a4ed73068304ad19143ce6a60616a754dfd0
Alisson Rangel <alisson.rangel@hotmail.com>
2021-04-23 14:05:07 -0300

    10.1, partial post_card e posts na timeline do user

--- app/controllers/user/timeline_controller.rb
+++ app/controllers/user/timeline_controller.rb
@@ -1,6 +1,8 @@
 class User::TimelineController < UserController
 
     def index
-        
+        ids = current_user.following.pluck(:id)
+        ids << current_user.id
+        @posts = Post.where(user_id: ids).order(created_at: :desc)
     end
 end
