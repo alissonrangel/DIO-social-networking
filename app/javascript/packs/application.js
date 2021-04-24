1c6f3c75c80da808b3ebe44554d36176dbe1dd94
Alisson Rangel <alisson.rangel@hotmail.com>
2021-04-24 13:59:06 -0300

    12.5, problema com o js na hora de atualizar a tela após enviar um comment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
 import "./like_toggle"
 
 import $ from 'jquery';
 global.$ = jQuery;
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
